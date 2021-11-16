726c1fcc7a9ffeac23b54981cc5d9829135b7760
Benjamin Russell <benjaminrobertrussell@gmail.com>
2021-11-16 00:24:50 -0500

    successfully pulled data from my schema, content, assets, api from graphcms / graphQL in a next.js environment

--- disney-plus-clone/pages/index.js
+++ disney-plus-clone/pages/index.js
@@ -1,4 +1,48 @@
-const Home = () => {
+import { gql, GraphQLClient} from 'graphql-request';
+
+export const getStaticProps = async () => {
+
+  const url = `${process.env.REACT_APP_GRAPHCMS_URL}`;
+  const graphQLClient = new GraphQLClient(url, {
+    headers: {
+      "Authorization" : `Bearer ${process.env.REACT_APP_GRAPHCMS_APIKEY}`
+    }
+  })
+  console.log('check:',`${process.env.REACT_APP_GRAPHCMS_URL}`);
+  console.log('check2:',`${process.env.REACT_APP_GRAPHCMS_APIKEY}`);
+
+  const query = gql`
+    query {
+      videos {
+        createdAt,
+        id,
+        title,
+        description,
+        seen,
+        slug,
+        tags,
+        thumbnail {
+          url
+        },
+        mp4 {
+          url
+        }
+      }
+    }
+  `
+
+  const data = await graphQLClient.request(query)
+  // console.log('data:', data);
+  const videos = data.videos;
+  return{
+    props: {
+      videos
+    }
+  }
+}
+
+const Home = ({videos}) => {
+  console.log('videos:', videos);
   return (
     <div>Hello there.</div>
   )
